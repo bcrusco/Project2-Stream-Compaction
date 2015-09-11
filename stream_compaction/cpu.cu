49ab51c84a3d578fecb9711bd35fc8caa0abdcf5
Bradley Crusco <bcrusco627@gmail.com>
2015-09-10 20:53:23 -0400

    Implemented CPU compacting using scan.

--- stream_compaction/cpu.cu
+++ stream_compaction/cpu.cu
@@ -1,4 +1,5 @@
 #include <cstdio>
+#include <cstdlib>
 #include "cpu.h"
 
 namespace StreamCompaction {
@@ -31,14 +32,41 @@ int compactWithoutScan(int n, int *odata, const int *idata) {
     return j;
 }
 
+void zeroArray(int n, int *a) {
+	for (int i = 0; i < n; i++) {
+		a[i] = 0;
+	}
+}
+
 /**
  * CPU stream compaction using scan and scatter, like the parallel version.
  *
  * @returns the number of elements remaining after compaction.
  */
 int compactWithScan(int n, int *odata, const int *idata) {
-    // TODO
-    return -1;
+	int *temp = (int*)malloc(n * sizeof(int));
+	zeroArray(n, temp);
+	int *scan_output = (int*)malloc(n * sizeof(int));
+	zeroArray(n, scan_output);
+
+	// Compute temporary array
+	for (int i = 0; i < n; i++) {
+		if (idata[i] != 0) {
+			temp[i] = 1;
+		}
+	}
+
+	// Run exclusive scan on the temporary array
+	scan(n, scan_output, temp);
+
+	// Scatter
+	for (int i = 0; i < n; i++) {
+		if (temp[i] == 1) {
+			odata[scan_output[i]] = idata[i];
+		}
+	}
+
+    return scan_output[n - 1] + 1;
 }
 
 }
